8f71f9be174183133c6a27625c96c13be73c35d2
Reshad Farid <reshadfar@gmail.com>
2013-11-20 15:55:04 +0100

    files changed

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local' => array('VirtualMint'), // Change this to your local machine hostname.
+    //'local' => array('VirtualMint'), // Change this to your local machine hostname.
+    'local' => array('101.84.231.pool.hsleiden.nl'), // Change this to your local machine hostname.
     'staging' => array('your-staging-machine-name'),
     'production' => array('your-production-machine-name'),
 

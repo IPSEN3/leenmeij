8ce76278c0b8c2cb6a549df58bbb461d74085a74
Reshad Farid <reshadfar@gmail.com>
2014-01-15 14:24:12 +0100

    lz

---
+++ app/lang/en/user/table.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	'id'                 => 'Reservation-ID',
+	'reservationdate'                 => 'Placed on',
+	'reservation_start_date'                 => 'Start date',
+	'reservation_end_date'                 => 'End date',
+	'carid'                 => 'Car-ID',
+	'brand'				=> 'Brand',
+	'status'                 => 'status',
+	'actions'                 => 'Action',
+
+);
